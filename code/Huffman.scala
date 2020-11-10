1e7a63f53b28153c3ae135152c1a9aed42e2ff3c
Samuel Chassot <samuel.chassot@epfl.ch>
2020-11-10 11:29:31 +0100

    Begin implementing huffmna

---
+++ code/Huffman.scala
@@ -0,0 +1,16 @@
+//Labs 01 Formal verification (CS-550, EPFL)
+//
+//Samuel Chassot 270955
+//Daniel Filipe Nunes Silva 275197
+
+import stainless.collection._
+import stainless.lang._
+import stainless.annotation._
+
+import stainless.equations._
+ 
+object Huffman {
+
+    
+ 
+}
