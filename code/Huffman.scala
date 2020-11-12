2b036473c89ef9ff63cd9a29b6c751c647fe5bdf
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2020-11-13 00:57:15 +0100

    clean code, add printHuffmanCode and Main functions

--- code/Huffman.scala
+++ code/Huffman.scala
@@ -29,12 +29,31 @@ object HuffmanCode {
 
   def insortTree(t: Tree, f: Forest): Forest = f match {
     case Nil() => List(t)
-    case Cons(t, ts) => if (cachedWeight(t) <= cachedWeight(t)) t :: t :: ts
-                        else t :: insortTree(t, ts)
+    case head :: tail => if (cachedWeight(t) <= cachedWeight(head)) t :: f else head :: insortTree(t, tail)
   }
 
   def huffmansAlgorithm(f: Forest): Tree = f match {
     case t1 :: t2 :: ts => huffmansAlgorithm(insortTree(uniteTrees(t1, t2), ts))
     case t :: _ => t
+  }
+
+  @extern
+  def printHuffmanCode(t: Tree): Unit = {
+    def printHuffmanCodeHelper(t: Tree, cw: String): Unit = t match {
+      case InnerNode(_, t1, t2) => {
+        printHuffmanCodeHelper(t1, cw.concat("0"))
+        printHuffmanCodeHelper(t2, cw.concat("1"))
+      }
+      case Leaf(_, c) => {
+        println(s"$c : $cw")
+      }
+    }
+
+    printHuffmanCodeHelper(t, "")
+  }
+ 
+  @extern
+  def main(args: Array[String]): Unit = {
+    printHuffmanCode(huffmansAlgorithm(List(Leaf(1, 'a'), Leaf(3, 'b'), Leaf(4, 'c'), Leaf(12, 'd'))))
   }
 }
