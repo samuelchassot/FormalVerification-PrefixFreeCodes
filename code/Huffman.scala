a54e5456277429c8be73054dbc7e21bd6bae3645
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2020-11-13 00:02:09 +0100

    review code skeleton

--- code/Huffman.scala
+++ code/Huffman.scala
@@ -6,43 +6,35 @@
 import stainless.collection._
 import stainless.lang._
 import stainless.annotation._
-
 import stainless.equations._
- 
-object Huffman {
+import stainless.proof.check
 
-    def test() = {
+object HuffmanCode {
 
-    }.ensuring( _ => true)
-    
- 
-}
+  type Alphabet = Char
+  type Forest = List[Tree]
+  
+  sealed abstract class Tree {
+  }
 
-object  HuffmanTree{
-    type Alphabet = Byte
-    type Forest = List[HuffmanTree]
-    sealed abstract class HuffmanTree {
+  case class InnerNode(w: Int, t1: Tree, t2: Tree) extends Tree
+  case class Leaf(w: Int, c: Alphabet) extends Tree
 
-    }
-    case class Leaf(w: Int, c: Alphabet) extends HuffmanTree
-    case class InnerNode(w: Int, t1: HuffmanTree, t2: HuffmanTree) extends HuffmanTree
+  def cachedWeight(t: Tree): Int = t match {
+    case InnerNode(w, t1, t2) => w
+    case Leaf(w, c) => w
+  }
 
-    def cachedWeight(tree: HuffmanTree): Int = tree match {
-            case InnerNode(w, t1, t2) => w
-            case Leaf(w, c) => w
-        }
+  def uniteTrees(t1: Tree, t2: Tree): Tree = InnerNode(cachedWeight(t1) + cachedWeight(t2), t1, t2)
 
-    def uniteTrees(t1: HuffmanTree, t2: HuffmanTree): HuffmanTree = InnerNode(cachedWeight(t1) + cachedWeight(t2), t1, t2)
+  def insortTree(t: Tree, f: Forest): Forest = f match {
+    case Nil() => List(t)
+    case Cons(t, ts) => if (cachedWeight(t) <= cachedWeight(t)) t :: t :: ts
+                        else t :: insortTree(t, ts)
+  }
 
-    def insortTree(tree: HuffmanTree, forest: Forest): Forest = forest match {
-        case Nil() => tree :: Nil()
-        case head :: tl => if (cachedWeight(tree) <= cachedWeight(head)) tree :: head :: tl 
-                            else head :: insortTree(tree, tl)
-    }
-
-    def huffmanAlgorithm(forest: Forest) : HuffmanTree = forest match {
-        case t :: Nil() => t
-        case t1 :: t2 ::  tl => huffmanAlgorithm(insortTree(uniteTrees(t1, t2), tl))
-    }
-    
+  def huffmansAlgorithm(f: Forest): Tree = f match {
+    case t1 :: t2 :: ts => huffmansAlgorithm(insortTree(uniteTrees(t1, t2), ts))
+    case t :: _ => t
+  }
 }
