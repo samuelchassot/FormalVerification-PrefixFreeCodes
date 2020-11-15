b5360fa1084a11092750c42b6dd84304cc2397cb
Samuel Chassot <samuel.chassot@epfl.ch>
2020-11-15 16:13:08 +0100

    implement decode/encode

--- code/Huffman.scala
+++ code/Huffman.scala
@@ -11,16 +11,18 @@ import stainless.proof.check
 
 object HuffmanCode {
 
+  // Tree -----------------------------------------------------------
+
   type Alphabet = Char
   type Forest = List[Tree]
   
   sealed abstract class Tree {
   }
 
-  case class InnerNode(w: Int, t1: Tree, t2: Tree) extends Tree
-  case class Leaf(w: Int, c: Alphabet) extends Tree
+  case class InnerNode(w: BigInt, t1: Tree, t2: Tree) extends Tree
+  case class Leaf(w: BigInt, c: Alphabet) extends Tree
 
-  def cachedWeight(t: Tree): Int = t match {
+  def cachedWeight(t: Tree): BigInt = t match {
     case InnerNode(w, t1, t2) => w
     case Leaf(w, c) => w
   }
@@ -37,6 +39,68 @@ object HuffmanCode {
     case t :: _ => t
   }
 
+  // ------------------------------------------------------------------
+  // Token ------------------------------------------------------------
+
+  sealed abstract class Token
+
+  case class ValidToken(bits: List[Boolean]) extends Token
+  case class ElementNotFoundToken() extends Token
+
+  def isValidToken(t: Token): Boolean = t match {
+    case ElementNotFoundToken() => false
+    case ValidToken(_) => true
+  }
+
+  def encodeElement(t: Tree, elmt: Alphabet, acc: List[Boolean]): Token = t match {
+    case Leaf(w, c) => if (c == elmt) ValidToken(acc) else ElementNotFoundToken()
+    case InnerNode(w, t1, t2) => encodeElement(t1, elmt, acc ++ List(false)) match {
+      case ValidToken(bits) => ValidToken(bits)
+      case ElementNotFoundToken() => encodeElement(t2, elmt, acc ++ List(true)) 
+    }
+  }
+  def encode(t: Tree, content: List[Alphabet]): List[Token] = content match {
+    case Nil() => Nil()
+    case head :: tl => encodeElement(t, head, Nil()) :: encode(t, tl)
+  }
+  
+  def decodeElement(t: Tree, token: Token): Option[Alphabet] = {
+    
+    def decodeElementHelper(t: Tree, bits: List[Boolean]): Option[Alphabet] = t match {
+      case Leaf(w, c) => if(bits.size == 0) Some(c) else None()
+      case InnerNode(w, t1, t2) => {
+        bits match {
+          case Nil() => None()
+          case head :: tl => if(!head) decodeElementHelper(t1, tl) else decodeElementHelper(t2, tl)
+        }
+      }
+    }
+    token match {
+      case ElementNotFoundToken() => None()
+      case ValidToken(bits) => decodeElementHelper(t, bits)
+    }
+    
+  }
+  
+  def decode(t: Tree, tokens: List[Token]): Option[List[Alphabet]] = {
+    require(isValidListOfTokens(tokens))
+    tokens match {
+      case Nil() => Some(Nil())
+      case head :: tl => decodeElement(t, head) match {
+        case Some(elmt) => decode(t, tl) match {
+          case Some(value) => Some(elmt :: value)
+          case None() => None()
+        }
+        case None() => None()
+      } 
+    }
+}
+
+  def isValidListOfTokens(l: List[Token]): Boolean = {
+    l.forall(isValidToken)
+  }
+  
+ // Token ------------------------------------------------------------
   @extern
   def printHuffmanCode(t: Tree): Unit = {
     def printHuffmanCodeHelper(t: Tree, cw: String): Unit = t match {
