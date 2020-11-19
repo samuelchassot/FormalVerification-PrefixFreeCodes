62f0369bc4507a3132564abb5609202888db27b3
Daniel Filipe Nunes Silva <danielementary@gmail.com>
2020-11-20 00:12:47 +0100

    comment code

--- code/Huffman.scala
+++ code/Huffman.scala
@@ -34,16 +34,16 @@ object HuffmanCode {
     case Leaf(_, _) => true
   }
 
-  // return the weight of a tree------------------------------------------------
+  // return the weight of a Tree------------------------------------------------
   def cachedWeight(t: Tree): BigInt = t match {
     case InnerNode(w, t1, t2) => w
     case Leaf(w, c) => w
   }
 
-  // merge two trees in one by adding an innernode------------------------------
+  // merge two Tree in one by adding an InnerNode-------------------------------
   def uniteTrees(t1: Tree, t2: Tree): Tree = InnerNode(cachedWeight(t1) + cachedWeight(t2), t1, t2)
 
-  // insert a tree in a forest and sort the latter------------------------------
+  // insert a Tree in a Forest and sort the latter------------------------------
   def insortTree(t: Tree, f: Forest): Forest = {
     decreases(f.length)
 
@@ -53,7 +53,7 @@ object HuffmanCode {
     }
   }
 
-  // prove insortTree increases the forest size by 1----------------------------
+  // prove insortTree increases the Forest size by 1----------------------------
   def insortTreeLength(t: Tree, f: Forest): Unit = {
     f match {
       case Nil() => ()
@@ -69,23 +69,23 @@ object HuffmanCode {
     }
   }.ensuring(_ => insortTree(t, f).length == f.length+1)
 
-  // generate the forest of leaves for a given list of characters---------------
+  // generate the Forest of Leaf for a given list of characters-----------------
   def generateUnsortedForest(s: List[Char]): Forest = {
     s.foldLeft[List[Char]](Nil())((l, c) => if (l.contains(c)) l else (c :: l)).map(c => Leaf(s.count(_ == c), c))
   }
 
-  // sort a forest--------------------------------------------------------------
+  // sort a Forest--------------------------------------------------------------
   def sortForest(f: Forest): Forest = f match {
       case Nil() => Nil()
       case hd :: tl => insortTree(hd, sortForest(tl))
   }
 
-  // generate and sort a forest given a list of characters----------------------
+  // generate and sort a Forest given a list of characters----------------------
   def generateSortedForest(s: List[Char]): Forest = {
     sortForest(generateUnsortedForest(s))
   }
 
-  // generate Huffman code's tree given a forest--------------------------------
+  // generate Huffman code's Tree given a Forest--------------------------------
   def huffmansAlgorithm(f: Forest): Tree = {
     require(!f.isEmpty)
     decreases(f.length)
@@ -101,7 +101,7 @@ object HuffmanCode {
 
   // encode/decode--------------------------------------------------------------
 
-  // to complete----------------------------------------------------------------
+  // encode a character as a list of bits with a given tree---------------------
   def encodeChar(t: Tree, c: Char, acc: List[Boolean]): List[Boolean] = {
     t match {
       case Leaf(_, lC) => if (lC == c) acc else Nil()
@@ -114,20 +114,20 @@ object HuffmanCode {
     }
   }
 
-  // to complete----------------------------------------------------------------
-  def encodeHelper(t: Tree, s: List[Char]) : List[Boolean] = {
+  // encode a list of chararcters with a given tree recursively-----------------
+  def encodeHelper(t: InnerNode, s: List[Char]) : List[Boolean] = {
     s match {
       case Nil() => Nil()
       case hd :: tl => encodeChar(t, hd, Nil()) ++ encodeHelper(t, tl)
     }
   }
 
-  // to complete----------------------------------------------------------------
-  def encode(t: Tree, s: List[Char]): List[Boolean] = {
+  // encode a list of characters as list of bits with a given tree--------------
+  def encode(t: InnerNode, s: List[Char]): List[Boolean] = {
     encodeHelper(t, s)
   }
   
-  // to complete----------------------------------------------------------------
+  // check if at least one character can be decoded-----------------------------
   def canDecodeAtLeastOneChar(t: Tree, bs: List[Boolean]): Boolean = {
     require(isInnerNode(t))
 
@@ -145,7 +145,7 @@ object HuffmanCode {
     }}}
   }
 
-  // to complete----------------------------------------------------------------
+  // check if the whole list of bits can be correctly decoded-------------------
   def canDecode(s: Tree, bs: List[Boolean])(implicit t: InnerNode): Boolean = {
     require(isInnerNode(s))
     decreases(bs.length)
@@ -164,18 +164,26 @@ object HuffmanCode {
     }}}
   }
 
+  // prove that canDecode implies canDecodeAtLeastOneChar-----------------------
   def canDecodeImpliesCanDecodeAtLeastOneChar(t: InnerNode, bs: List[Boolean]): Unit = {
     require(canDecode(t, bs)(t))
+
     ()
+
+    //TODO
   }.ensuring(_ => canDecodeAtLeastOneChar(t, bs))
 
+  // prove that can decode implies that we can decode the remaining bits--------
+  // after having decoded the first decodable character-------------------------
   def canDecodeImpliesCanDecodeTailAfterOneCharDecoded(t: InnerNode, bs: List[Boolean]): Unit = {
     require(canDecode(t, bs)(t))
 
     canDecodeImpliesCanDecodeAtLeastOneChar(t, bs)
+
+    //TODO
   }.ensuring(_ => decodeChar(t, bs) match { case(_, nBs) => if (nBs.isEmpty) true else canDecode(t, nBs)(t) })
 
-  // to complete----------------------------------------------------------------
+  // decode a single character from a list of bits with a given tree------------
   def decodeChar(t: Tree, bs: List[Boolean]): (Char, List[Boolean]) = {
     require(isInnerNode(t) && canDecodeAtLeastOneChar(t, bs))
 
@@ -192,12 +200,17 @@ object HuffmanCode {
     }}}
   }
 
+  // prove that the length of the remaining list of bits after decoding---------
+  // the first decodable character is smaller than the original list of bits----
   def decodeCharLength(t: Tree, bs: List[Boolean]): Unit = {
     require(isInnerNode(t) && canDecodeAtLeastOneChar(t, bs))
+
     ()
+
+    //TODO
   }.ensuring(_ => decodeChar(t, bs) match { case (_, nBs) => nBs.length < bs.length })
 
-  // to complete----------------------------------------------------------------
+  // decode a list of bits with a given tree recursively------------------------
   def decodeHelper(t: InnerNode, bs: List[Boolean], acc: List[Char]): List[Char] = {
     require(!bs.isEmpty && canDecode(t, bs)(t))
     decreases(bs.length)
@@ -208,7 +221,7 @@ object HuffmanCode {
     decodeChar(t, bs) match { case(c, nBs) => if (nBs.isEmpty) acc else decodeHelper(t, nBs, acc ++ List(c)) }
   }
 
-  // to complete----------------------------------------------------------------
+  // decode a list of bits as a list of characters with a given tree------------
   def decode(t: InnerNode, bs: List[Boolean]): List[Char] = {
     require(canDecode(t, bs)(t))
     decreases(bs.length)
